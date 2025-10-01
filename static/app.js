f231638bbb746f224928e74d58b83e77d4fe7f6c
Mr-Kam <kanicetcyrille@gmail.com>
2025-10-01 18:09:00 +0000

    Update

--- static/app.js
+++ static/app.js
@@ -75,6 +75,7 @@ const CONFIG = {
     targetDate: "2025-10-04 10:00:00",      // Date/heure d'ouverture de la classe
     endDate: "2025-10-04 12:00:00",         // Date/heure de fermeture de la classe (optionnel)
     classUrl: "https://meet.jit.si/CasualWarmingsDepictNeither",
+    // lien: https://meet.jit.si/formationpluspythonvisualisation
     redirectUrl: "index.html"
 };
 

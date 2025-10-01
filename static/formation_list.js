74714ad61bcb58b6ce27a006b971316e396ad141
Mr-Kam <kanicetcyrille@gmail.com>
2025-10-01 19:07:04 +0000

    Formation en ligne added

---
+++ static/formation_list.js
@@ -0,0 +1,30 @@
+const formation_list = {
+
+   1:{ 
+    "Title":"Introduction a la visualisation de données avec python",
+    "img":"IMG/intro_data_viz_python.png",
+    "type":"youtube-link",
+    "link":"https://youtu.be/jzcixJChx3w",
+    "price:": "free",
+    "resume":"Apprenez depuis zéro les bases de la création de graphiques avec Python et Matplotlib. Une formation pratique et progressive pour visualiser vos données efficacement.",
+    "status":"accessible",
+    "keyword":"python,matplotlib,pandas,anaconda,jupyter,notebook,chart,visualisation de données",
+    "certification":false,
+    "teacher":"Anicet Cyrille Kambou",
+    "teacher_description":"Enseigant-chercheur | ESA/INP-HB"
+},
+2:{
+    "Title":"Modélisation 3D pour l'impression avec python",
+    "img":"IMG/modelisation_3D_python_openscad.png",
+    "type":"youtube-link",
+    "link":"",
+    "price:": "free",
+    "resume":"Apprenez à transformer une idée en un objet concret grâce à la modélisation 3D, prêt pour l’impression. Apprenez à le faire avec python dans toutes sa simplicité et Openscad",
+    "status":"not-added",
+    "keyword":"python,openscad,solidpython,anaconda,notebook,3D,modélisation, printing,cad",
+    "certification":false,
+    "teacher":"Anicet Cyrille Kambou",
+    "teacher_description":"Enseigant-chercheur | ESA/INP-HB"
+}
+
+}
